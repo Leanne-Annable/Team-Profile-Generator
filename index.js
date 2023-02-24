318015d2749314d8775c28621a25b87c6c74814c
Litttle-panda <117175961+Litttle-panda@users.noreply.github.com>
2023-02-24 16:45:59 +0000

    added questions and prompt layout for each role

--- index.js
+++ index.js
@@ -38,3 +38,105 @@ const managerQuestions = [
     },
 ];
 
+const staffOptions = {
+    type: "list",
+    message: "What type of team member would you like to add next?",
+    name: "option",
+    choices: [
+        "Engineer",
+        "Intern",
+        "Finished building the team",
+    ],
+};
+
+const engineerQuestions = [
+    {
+        type:"input",
+        message: "What is the Engineers name?",
+        name: "engineerName",
+    },
+    {
+        type:"input",
+        message: "What is the Engineers ID?",
+        name: "engineerID",
+    },
+    {
+        type:"input",
+        message: "What is the Engineers email?",
+        name: "engineerEmail",
+    },
+    {
+        type:"input",
+        message: "What is the Engineers GitHub name?",
+        name: "engineerGithub",
+    },
+]
+
+const internQuestions = [
+    {
+        type:"input",
+        message: "What is the Intern's name?",
+        name: "internName",
+    },
+    {
+        type:"input",
+        message: "What is the Intern's ID?",
+        name: "internID",
+    },
+    {
+        type:"input",
+        message: "What is the Intern's email?",
+        name: "internEmail",
+    },
+    {
+        type:"input",
+        message: "Which school does the Intern attend?",
+        name: "internSchool",
+    },
+];
+
+
+// TODO: Write Code to gather information about the development team members, and render the HTML file.
+
+inquirer.prompt([{
+    managerQuestions
+}]).then(response => {
+    // populate manager info
+    // promptForNexEMployee ()
+})
+
+const promptForNextEmployee = () => {
+    inquirer.prompt([{
+        staffOptions
+    }]).then(response => {
+        // if engineer
+        //    promptForEngineer
+        // else if intern
+        //    promptForIntern
+        // else
+        //    use the functionality from page-template to generate the team
+    })
+}
+
+const promptForEngineer = () => {
+    inquirer.prompt([{
+        engineerQuestions
+    }]).then(response => {
+        // add new engineer to employees array
+        // promptForNextEmployee
+    })
+}
+
+const promptForIntern = () => {
+    inquirer.prompt([{
+        internQuestions
+    }]).then(response => {
+        // add new intern to employees array
+        // promptForNextEmployee
+    })
+}
+
+const buildPage = () => {
+    // need to do fs.writefile after cheking if the file exists or not
+    // file location >> ("./src/page-template.js")
+};
