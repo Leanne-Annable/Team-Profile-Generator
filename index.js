5e06e166d463da0702e60c17bcc9ca6f3f6815c7
Litttle-panda <117175961+Litttle-panda@users.noreply.github.com>
2023-02-27 15:42:49 +0000

    added functions to call the manager and employee info and create the html page. CSS not showing correctly

--- index.js
+++ index.js
@@ -17,22 +17,22 @@ const companyEmployees = [];
 
 const managerQuestions = [
     {
-        type:"input",
+        type: "input",
         message: "What is the managers name?",
         name: "managerName",
     },
     {
-        type:"input",
+        type: "input",
         message: "What is the managers ID?",
         name: "managerID",
     },
     {
-        type:"input",
+        type: "input",
         message: "What is the managers email?",
         name: "managerEmail",
     },
     {
-        type:"input",
+        type: "input",
         message: "What is the managers office number?",
         name: "managerOfficeNumber",
     },
@@ -51,22 +51,22 @@ const staffOptions = {
 
 const engineerQuestions = [
     {
-        type:"input",
+        type: "input",
         message: "What is the Engineers name?",
         name: "engineerName",
     },
     {
-        type:"input",
+        type: "input",
         message: "What is the Engineers ID?",
         name: "engineerID",
     },
     {
-        type:"input",
+        type: "input",
         message: "What is the Engineers email?",
         name: "engineerEmail",
     },
     {
-        type:"input",
+        type: "input",
         message: "What is the Engineers GitHub name?",
         name: "engineerGithub",
     },
@@ -74,22 +74,22 @@ const engineerQuestions = [
 
 const internQuestions = [
     {
-        type:"input",
+        type: "input",
         message: "What is the Intern's name?",
         name: "internName",
     },
     {
-        type:"input",
+        type: "input",
         message: "What is the Intern's ID?",
         name: "internID",
     },
     {
-        type:"input",
+        type: "input",
         message: "What is the Intern's email?",
         name: "internEmail",
     },
     {
-        type:"input",
+        type: "input",
         message: "Which school does the Intern attend?",
         name: "internSchool",
     },
@@ -97,46 +97,91 @@ const internQuestions = [
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
-
-inquirer.prompt([{
-    managerQuestions
-}]).then(response => {
+function startManager() {
     // populate manager info
-    // promptForNexEMployee ()
-})
+    inquirer.prompt(managerQuestions).then((response) => {
+        companyEmployees.push(
+            new Manager(
+                response.managerName,
+                response.managerID,
+                response.managerEmail,
+                response.managerOfficeNumber,
+            )
+        );
+        // prompt for next employee
+        promptForNextEmployee();       
+    });
+};
 
-const promptForNextEmployee = () => {
-    inquirer.prompt([{
-        staffOptions
-    }]).then(response => {
-        // if engineer
+// use if statements to check which option is selected, then activate the relevant function
         //    promptForEngineer
         // else if intern
         //    promptForIntern
         // else
         //    use the functionality from page-template to generate the team
-    })
-}
+function promptForNextEmployee() {
+    inquirer.prompt(staffOptions).then((response) => {
+        if (response.option === "Engineer"){
+            promptForEngineer();
+        } else if (response.option === "Intern"){
+            promptForIntern();
+        } else {
+            console.log("Building your team page now.")
+            // buildPage();
+            if (!fs.existsSync(OUTPUT_DIR)){
+                fs.mkdirSync(OUTPUT_DIR);
+            };
+            // checks for error codes and notifies user
+            fs.writeFile(outputPath, render(companyEmployees), (err) =>
+            err ? console.log(err) : console.log("Employees Added")
+            );
+        };
 
-const promptForEngineer = () => {
-    inquirer.prompt([{
-        engineerQuestions
-    }]).then(response => {
-        // add new engineer to employees array
-        // promptForNextEmployee
-    })
-}
-
-const promptForIntern = () => {
-    inquirer.prompt([{
-        internQuestions
-    }]).then(response => {
-        // add new intern to employees array
-        // promptForNextEmployee
-    })
-}
-
-const buildPage = () => {
-    // need to do fs.writefile after cheking if the file exists or not
-    // file location >> ("./src/page-template.js")
+    });
 };
+
+function promptForEngineer() {
+    inquirer.prompt(engineerQuestions).then((response) => {
+        // add new engineer to employees array
+        companyEmployees.push(
+            new Engineer(
+                response.engineerName,
+                response.engineerID,
+                response.engineerEmail,
+                response.engineerGithub,
+            )
+        );
+        // console.log("Engineer created!")
+        promptForNextEmployee();        
+    });
+};
+
+function promptForIntern() {
+    inquirer.prompt(internQuestions).then((response) => {
+        // add new intern to employees array
+        companyEmployees.push(
+            new Intern(
+                response.internName,
+                response.internID,
+                response.internEmail,
+                response.internSchool,
+            )
+        );
+        promptForNextEmployee();
+    });
+};
+
+// function buildPage() {
+//     // need to do fs.writefile after cheking if the file exists or not
+//     // file location >> render = ("./src/page-template.js")
+//     if (!fs.existsSync(OUTPUT_DIR)){
+//         fs.mkdirSync(OUTPUT_DIR);
+//     };
+//     // checks for error codes and notifies user
+//     fs.writeFile(outputPath, render(companyEmployees), (err) =>
+//     err ? console.log(err) : console.log("Employees Added")
+//     );
+// };
+
+// call the manager function to start the process
+startManager();
